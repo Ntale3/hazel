174ca5bb79641c5aedfcb8a1e2b350124e4db7ef
Makisuo <minetoaster12@gmail.com>
2025-05-29 22:49:14 +0200

    temp build ifx

--- apps/convex/src/routes/_protected/_app/$serverId/layout.tsx
+++ apps/convex/src/routes/_protected/_app/$serverId/layout.tsx
@@ -4,7 +4,6 @@ import type { Id } from "convex-hazel/_generated/dataModel"
 import { createEffect, createMemo } from "solid-js"
 import { Sidebar } from "~/components/ui/sidebar"
 import { removeCurrentServerId, setCurrentServerId } from "~/lib/helpers/localstorage"
-import { AppSidebar } from "./-components/app-sidebar"
 
 export const Route = createFileRoute("/_protected/_app/$serverId")({
 	component: RouteComponent,
