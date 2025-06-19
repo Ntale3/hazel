ebea05ece9b05efd2af60cd3847ea92aa3ea7252
Makisuo <minetoaster12@gmail.com>
2025-06-20 00:50:30 +0200

    fix

--- apps/web/src/components/ui/user-avatar.tsx
+++ apps/web/src/components/ui/user-avatar.tsx
@@ -1,14 +1,16 @@
 import { Match, Switch } from "solid-js"
+import { cn } from "~/lib/utils"
 import { Avatar } from "./avatar"
 
 export const UserAvatar = (props: {
 	avatarUrl: string
 	displayName: string
 	status: "online" | "offline" | "away"
+	class?: string
 }) => {
 	return (
-		<div class="relative">
-			<Avatar class="size-7" src={props.avatarUrl} name={props.displayName} />
+		<div class={cn("relative size-max")}>
+			<Avatar class={cn("size-7", props.class)} src={props.avatarUrl} name={props.displayName} />
 			<Switch>
 				<Match when={props.status === "online"}>
 					<span class="-end-1 -bottom-1 absolute size-3 rounded-full border-2 border-background bg-emerald-500">
