28b5825a67d69ca0ce337926559d338ddbbe8d56
Makisuo <minetoaster12@gmail.com>
2025-12-17 00:08:01 +0100

    fix: react aria  router provider

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,15 +1,31 @@
 import { TanStackDevtools } from "@tanstack/react-devtools"
-import { createRootRouteWithContext, Outlet, useRouter } from "@tanstack/react-router"
+import {
+	createRootRouteWithContext,
+	type NavigateOptions,
+	Outlet,
+	type ToOptions,
+	useRouter,
+} from "@tanstack/react-router"
 import { RpcDevtoolsPanel } from "effect-rpc-tanstack-devtools/components"
 import { RouterProvider } from "react-aria-components"
 import { VersionCheck } from "~/components/version-check"
+
+declare module "react-aria-components" {
+	interface RouterConfig {
+		href: ToOptions
+		routerOptions: Omit<NavigateOptions, keyof ToOptions>
+	}
+}
 
 export const Route = createRootRouteWithContext<{}>()({
 	component: () => {
 		const router = useRouter()
 
 		return (
-			<RouterProvider navigate={(to, options) => router.navigate({ to, ...options })}>
+			<RouterProvider
+				navigate={(href, opts) => router.navigate({ ...href, ...opts })}
+				useHref={(href) => router.buildLocation(href).href}
+			>
 				{import.meta.env.DEV && (
 					<TanStackDevtools
 						plugins={[
